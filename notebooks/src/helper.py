b71153dfed766804979114d98e09c3f361885b90
krishnanjaliiyersr-lang <krishnanjaliiyersr@gmail.com>
2026-05-15 11:47:40 +0530

    Added notebooks

---
+++ notebooks/src/helper.py
@@ -0,0 +1,14 @@
+import yaml
+from pathlib import Path
+from os.path import join
+
+
+
+
+CONFIG_PATH ="config"
+config_name = "config. yaml"
+
+def load_config(config_name=config_name):
+    with open(CONFIG_PATH / config_name, "r") as f:
+        config = yaml.safe_load(f)
+    return config
